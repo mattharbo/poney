55ae46bcea79ce3c11a0430be435a5da53aabf62
matthieuharbonnier <matthieu.harbonnier@coorpacademy.com>
2016-03-14 12:49:13 +0100

    Update json

--- web/index.php
+++ web/index.php
@@ -7,11 +7,43 @@ $username=$_POST["user_name"];
 $userid=$_POST["user_id"];
 $text = $_POST['text'];
 
+########################################################################################################################################################
+
+$stats = "./hnG9yd4m1yl4nZM0SZjz3uzl.json";
+
+$json = file_get_contents($stats); 
+$data = json_decode($json,true);
+$tocomplete = 0;
+$index = 0;
+$todaysdate=date('Ymd');
+
+foreach ($data['users'] as $user) {
+	++$index;
+	if ($user['username']==$username){
+		++$tocomplete;
+		$match=$index-1;
+	}
+}
+
+if ($tocomplete==1) {
+	$data['users'][$match]['numberofhack']=$data['users'][$match]['numberofhack']+1;
+	array_push($data['users'][$match]['hackdetails'], array('date'=>$todaysdate, 'associatedtext'=> $text));
+}else{
+	array_push($data['users'], array('id' => $userid, 'username' => $username, 'numberofhack'=>'1', 'hackdetails' => array(array('date'=> $todaysdate, 'associatedtext' => $text))));
+}
+
+unset($file);//prevent memory leaks for large json.
+//save the file
+file_put_contents('./hnG9yd4m1yl4nZM0SZjz3uzl.json',json_encode($data));
+unset($data);//release memory
+
+########################################################################################################################################################
+
 $jsonData = [
 	"response_type" => "in_channel",//if you want to set this message to private
 	"text" => "@channel : ".$username." has been *poneyhacked* :smirk: ".$text,
 	'attachments' => [[
-		'text' => 'Text 1',
+		'text' => 'User table :champagne:',
 		'color' => '#F35A00'  
 	]]//end attachments
 ];
--- web/test.php
+++ web/test.php
@@ -1,36 +1,35 @@
 <?
 
-$newhackedname="matthieu";
+$newhackedname="MHA";
 
 $stats = "./hnG9yd4m1yl4nZM0SZjz3uzl.json";
 
 $json = file_get_contents($stats); 
 $data = json_decode($json,true);
-$match = 0;
+$tocomplete = 0;
+$index = 0;
+$todaysdate=date('Ymd');
 
 foreach ($data['users'] as $user) {
+	++$index;
 	if ($user['username']==$newhackedname){
-		++$match;
-		$user['numberofhack']=0;
+		++$tocomplete;
+		$match=$index-1;
 	}
+}
+
+if ($tocomplete==1) {
+	$data['users'][$match]['numberofhack']=$data['users'][$match]['numberofhack']+1;
+	array_push($data['users'][$match]['hackdetails'], array('date'=>$todaysdate, 'associatedtext'=>'text'));
+}else{
+	array_push($data['users'], array('id' => 'new id', 'username' => 'test username', 'numberofhack'=>'1', 'hackdetails' => array(array('date'=> 'test date', 'associatedtext' => 'test associated text'))));
 }
 
 print_r($data);
 
-echo "is there a match ? ".$match;
-if ($match==1) {
-	echo "<br>Number of hack for the user match ".$data['users'][0]['numberofhack'];
-}
-
-
-// unset($file);//prevent memory leaks for large json.
-// //insert data here
-// // $data['users'][0]['id'] = 007;
-// // $data['users'][0]['id'] = array('data'=>'some data');
-// //save the file
-// file_put_contents('./hnG9yd4m1yl4nZM0SZjz3uzl.json',json_encode($data));
-// unset($data);//release memory
-
-
+unset($file);//prevent memory leaks for large json.
+//save the file
+file_put_contents('./hnG9yd4m1yl4nZM0SZjz3uzl.json',json_encode($data));
+unset($data);//release memory
 
 ?>
