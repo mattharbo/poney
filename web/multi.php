6e53e9490a9010ad8fca155f0bd760dc73b8e5e5
matthieuharbonnier <matthieu.harbonnier@coorpacademy.com>
2016-03-15 14:30:05 +0100

    champagne test

---
+++ web/multi.php
@@ -0,0 +1,11 @@
+<?
+
+$number="1";
+
+if ($number % 3 == 0 and $number != 1) {
+	echo "multiple of 3";
+}else{
+	echo "is not";
+}
+
+?>
--- web/test.php
+++ web/test.php
@@ -8,6 +8,7 @@ $json = file_get_contents($stats);
 $data = json_decode($json,true);
 $tocomplete = 0;
 $index = 0;
+$champagne = 0;
 $todaysdate=date('Ymd_h:i:s');
 
 foreach ($data['users'] as $user) {
@@ -20,6 +21,11 @@ foreach ($data['users'] as $user) {
 
 if ($tocomplete==1) {
 	$data['users'][$match]['numberofhack']=$data['users'][$match]['numberofhack']+1;
+
+	if ($data['users'][$match]['numberofhack'] % 3 == 0 and $data['users'][$match]['numberofhack'] != 1) {
+		$champagne = 1;
+	}
+
 	array_push($data['users'][$match]['hackdetails'], array('date'=>$todaysdate, 'associatedtext'=>'blabla'));
 }else{
 	array_push($data['users'], array('id' => 'x', 'username' => $newhackedname, 'numberofhack'=>'1', 'hackdetails' => array(array('date'=> $todaysdate, 'associatedtext' => 'x'))));
@@ -30,6 +36,10 @@ $arraytosort=array();
 foreach ($data['users'] as $user2) {
 	$arraytosort[$user2['username']] = $user2['numberofhack'];
 	arsort($arraytosort);
+}
+
+if ($champagne ==1) {
+	$concattext = ":champagne:";
 }
 
 foreach ($arraytosort as $key => $value) {
